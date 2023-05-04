fa61c46d881a7eabbaface0b13e9e84563039c5c
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-05-04 22:39:20 +0100

    Feat: Add JavaScript for Dynamic Content
    I added some data to dynamically populate the tables in the recent
    orders section. That way, it is the dashboard it was designed to be.
    Still adding some dynamism in other areas but this is the starting.

---
+++ assets/orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '53678',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Range Rover Autobiography',
+        productNumber: '53679',
+        paymentStatus: 'Paid',
+        shipping: 'Completed'
+    },
+    {
+        productName: 'iPhone 14 Pro Max',
+        productNumber: '53680',
+        paymentStatus: 'Paid',
+        shipping: 'Completed'
+    },
+    {
+        productName: 'iPod 7th Gen',
+        productNumber: '53681',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+]
